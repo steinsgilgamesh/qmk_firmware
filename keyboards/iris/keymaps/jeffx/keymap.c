5a089fdc0ec14d06dd107e029b48c91c4ac94d7f
Jeff Xiao <jeffrey@ascent.ai>
2018-07-13 18:36:29 +0900

    Added a totally transparent layer boys

--- keyboards/iris/keymaps/jeffx/keymap.c
+++ keyboards/iris/keymaps/jeffx/keymap.c
@@ -77,22 +77,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, _______, _______, _______, _______, _______,               _______, _______, _______, _______, KC_MINS, KC_EQL,  _______ ,  
     _______, _______, _______, _______, _______, _______,                        _______, _______, _______, KC_MINS, KC_EQL,  _______ ,  
     _______, _______, _______, _______, _______, _______, _______       _______, _______, _______, _______, KC_MINS, KC_EQL,  _______ ,  
-    _______, _______, _______, _______, _______, _______, _______,      _______, _______, _______ 
-
-
-
-
-
-
-
-
-    _______, _______, _______, KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                           KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL  ,
-    KC_LSFT, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                           KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT  ,
-    KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_MPLY,      KC_MNXT, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT ,
-                                        KC_LGUI, KC_LALT, KC_SPC,       MO(_LW), MO(_RS), MO(_FN)
- ),
-
-  [_LW] = LAYOUT_kc(
+                                        _______, _______, _______,      _______, _______, _______ 
+ ), 
+    [_LW] = LAYOUT_kc(
   //,----+----+----+----+----+----.              ,----+----+----+----+----+----.
      TILD,EXLM, AT ,HASH,DLR ,PERC,               CIRC,AMPR,ASTR,LPRN,RPRN,BSPC,
   //|----+----+----+----+----+----|              |----+----+----+----+----+----|
