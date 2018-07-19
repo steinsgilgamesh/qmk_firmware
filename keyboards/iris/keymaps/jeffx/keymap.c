8d40d3b55a50a51db62ea37731e6a6591a4ceabb
Jeff Xiao <jeffrey@ascent.ai>
2018-07-19 20:05:08 +0900

    Fix a syntax error where I deleted two keys

--- keyboards/iris/keymaps/jeffx/keymap.c
+++ keyboards/iris/keymaps/jeffx/keymap.c
@@ -124,7 +124,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, _______, _______, _______, _______, _______,                        _______, _______, _______, KC_UNDS, KC_PLUS, _______ ,  
     _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                          _______, KC_PGDN, KC_PGUP, KC_LCBR, KC_RCBR, _______ ,  
     _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,                         KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, KC_PIPE, _______ ,  
-    _______, BL_TOGG, BL_STEP, RGB_HUD, KC_F11,  KC_F12,                         _______, _______, KC_LABK, KC_RABK, KC_QUES, _______ ,  
+    _______, BL_TOGG, BL_STEP, RGB_HUD, KC_F11,  KC_F12,  _______,      _______, _______, _______, KC_LABK, KC_RABK, KC_QUES, _______ ,  
                                         _______, _______, _______,      _______, _______, _______ 
  )
  
