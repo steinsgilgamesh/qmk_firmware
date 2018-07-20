062a3e5b6fc088cdf65f2eeb817d7e853c757522
Jeff Xiao <jeffrey@ascent.ai>
2018-07-20 19:57:18 +0900

    Progress commit from work

--- keyboards/iris/keymaps/jeffx/keymap.c
+++ keyboards/iris/keymaps/jeffx/keymap.c
@@ -68,7 +68,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                           KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL  ,
     KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                           KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT  ,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_MPLY,      KC_MNXT, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT ,
-                                        LM_FNOS, KC_LALT, KC_SPC,       MO(_LW), MO(_RS), TO(_FN) 
+                                        LM_FNOS, KC_LALT, KC_SPC,       MO(_LW), MO(_RS), TG(_FN) 
  ),
 
 
@@ -87,7 +87,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
  [_LW] = LAYOUT( 
-    _______, _______, _______, _______, _______, _______,                        _______, _______, _______, _______, _______, _______ ,  
+    KC_GRV,  _______, _______, _______, _______, _______,                        _______, _______, _______, _______, _______, _______ ,  
     _______, _______, _______, _______, _______, _______,                        _______, _______, _______, _______, _______, _______ ,  
     _______, _______, _______, _______, _______, KC_MINS,                        KC_EQL,  _______, _______, KC_QUOT, KC_BSLS, _______ ,  
     _______, _______, _______, _______, _______, KC_LBRC, _______,      _______, KC_RBRC, _______, KC_COMM, KC_DOT,  KC_SLSH, _______ ,  
@@ -110,7 +110,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
  [_RS] = LAYOUT( 
-    _______, _______, _______, _______, _______, _______,                        _______, _______, _______, _______, _______, _______ ,  
+    KC_TIL,  _______, _______, _______, _______, _______,                        _______, _______, _______, _______, _______, _______ ,  
     _______, _______, _______, _______, _______, _______,                        _______, _______, _______, _______, _______, _______ ,  
     _______, _______, _______, _______, _______, KC_UNDS,                        KC_PLUS, _______, _______, KC_DQUO, KC_PIPE, _______ ,  
     _______, _______, _______, _______, _______, KC_LCBR, _______,      _______, KC_RCBR, _______, KC_LABK, KC_RABK, KC_QUES, _______ ,  
