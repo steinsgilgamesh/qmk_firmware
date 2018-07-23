ffe9ab44897c9899ce79abf73c819e50d13cd945
Jeff Xiao <jeffrey@ascent.ai>
2018-07-23 19:33:19 +0900

    Fix a syntax error and try another variation of modifiers

--- keyboards/iris/keymaps/jeffx/keymap.c
+++ keyboards/iris/keymaps/jeffx/keymap.c
@@ -101,7 +101,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                           KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL  ,
     KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                           KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT  ,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_MPLY,      KC_MNXT, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT ,
-                                        LM_FNOS, KC_LALT, KC_SPC,       MO(_LW), MO(_RS), TG(_FN) 
+                                        LM_FNOS, KC_LALT, KC_SPC,       MO(_FN), MO(_LW), MO(_RS) 
  ),
 
 
@@ -122,8 +122,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  [_LW] = LAYOUT( 
     KC_GRV,  _______, _______, _______, _______, _______,                        _______, _______, _______, _______, _______, _______ ,  
     _______, _______, _______, _______, _______, _______,                        _______, _______, _______, _______, _______, _______ ,  
-    _______, _______, _______, _______, _______, KC_MINS,                        KC_EQL,  KC_MINS, KC_EQL,  KC_QUOT, KC_BSLS, _______ ,  
-    _______, _______, _______, _______, _______, KC_LBRC, _______,      _______, KC_RBRC, KC_EQL,  KC_LBRC, KC_RBRC, KC_SLSH, _______ ,  
+    _______, _______, _______, _______, _______, _______,                        _______, KC_MINS, KC_EQL,  KC_QUOT, KC_BSLS, _______ ,  
+    _______, _______, _______, _______, _______, _______, _______,      _______, _______, _______, KC_LBRC, KC_RBRC, KC_SLSH, _______ ,  
                                         _______, _______, _______,      _______, _______, _______ 
  ),
  
@@ -145,8 +145,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  [_RS] = LAYOUT( 
     KC_TIL,  _______, _______, _______, _______, _______,                        _______, _______, _______, _______, _______, _______ ,  
     _______, _______, _______, _______, _______, _______,                        _______, _______, _______, _______, _______, _______ ,  
-    _______, _______, _______, _______, _______, KC_UNDS,                        KC_PLUS, KC_UNDS, KC_PLUS, KC_DQUO, KC_PIPE, _______ ,  
-    _______, _______, _______, _______, _______, KC_LCBR, _______,      _______, KC_RCBR, _______, KC_LCBR, KC_RCBR, KC_QUES, _______ ,  
+    _______, _______, _______, _______, _______, _______,                        _______, KC_UNDS, KC_PLUS, KC_DQUO, KC_PIPE, _______ ,  
+    _______, _______, _______, _______, _______, _______, _______,      _______, _______, _______, KC_LCBR, KC_RCBR, KC_QUES, _______ ,  
                                         _______, _______, _______,      _______, _______, _______ 
  ),
 
@@ -166,8 +166,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
  [_FN] = LAYOUT( 
-    _______, RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI, _______,                        _______, _______, _______, KC_UNDS, KC_PLUS, _______ ,  
-    _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                          _______, KC_PGDN, KC_PGUP, KC_LCBR, KC_RCBR, _______ ,  
+    _______, RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI, _______,                        _______, _______, _______, _______, _______, _______ ,  
+    _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                          _______, KC_PGDN, KC_PGUP, _______, _______, _______ ,  
     _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,                         KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, KC_PIPE, _______ ,  
     _______, BL_TOGG, BL_STEP, RGB_TOG, KC_F11,  KC_F12,  _______,      _______, _______, _______, KC_LABK, KC_RABK, KC_QUES, _______ ,  
                                         _______, _______, _______,      _______, _______, _______ 
