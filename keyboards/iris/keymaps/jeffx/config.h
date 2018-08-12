476235036fa305bc9b9704d88f67e173214f36a9
jeffreykxiao <jeffrey@ascent.ai>
2018-08-12 11:31:00 +0900

    Attempt to put RGB and backlight to sleep

--- keyboards/iris/keymaps/jeffx/config.h
+++ keyboards/iris/keymaps/jeffx/config.h
@@ -38,5 +38,5 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define RGBLIGHT_SAT_STEP 8
 #define RGBLIGHT_VAL_STEP 8
 #define TAPPING_TERM 200
-
+#define RGBLIGHT_SLEEP
 #endif
