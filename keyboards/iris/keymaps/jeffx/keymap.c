a98460e054dbf2831b8827e02fc9585e0a77071a
jeffreykxiao <jeffrey@ascent.ai>
2018-08-18 21:06:38 +0900

    Gaming Layer lmao

--- keyboards/iris/keymaps/jeffx/keymap.c
+++ keyboards/iris/keymaps/jeffx/keymap.c
@@ -8,9 +8,10 @@
 extern keymap_config_t keymap_config;
 
 #define _QW 0
-#define _LW 1
-#define _RS 2
-#define _FN 3 
+#define _GM 1
+#define _LW 2
+#define _RS 3
+#define _FN 4
 
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
@@ -108,6 +109,28 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  ),
 
 
+  /* GAMING // This keyboard has number keys so it doesn't really need a gaming layer, but a right-handed FN key is useful here.
+ * .-----------------------------------------------------.                      .-----------------------------------------------------.
+ * | ESC    | 1      | 2      | 3      | 4      | 5      |                      | 6      | 7      | 8      | 9      | 0      | BACKSP |
+ * |--------+--------+--------+--------+--------+--------|                      |--------+--------+--------+--------+-----------------|
+ * | TAB    | Q      | W      | E      | R      | T      |                      | Y      | U      | I      | O      | P      | DEL    |
+ * |--------+--------+--------+--------+--------+--------|                      |--------+--------+--------+-----------------+--------|
+ * | LCTRL  | A      | S      | D      | F      | G      |                      | H      | J      | K      | L      | ;      | ENTER  |
+ * |--------+--------+--------+--------+--------+--------+--------.    .--------+--------+--------+--------------------------+--------|
+ * | LSHIFT | Z      | X      | C      | V      | B      | PLAY   |    | ????   | N      | M      | ,      | .      | /      | RSHIFT |
+ * '-----------------------------------+--------+--------+--------|    |--------+--------+--------+-----------------------------------'
+ *                                     | FUNC   | ALT    | SPACE  |    | LOWER  | RAISE  | FUNC   |
+ *                                     '--------------------------'    '--------------------------'
+ */
+
+ [_GM] = LAYOUT( 
+    KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                           KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC ,
+    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                           KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL  ,
+    KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                           KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT  ,
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_MPLY,      KC_MNXT, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT ,
+                                        MO(_FN), KC_LALT, KC_SPC,       MO(_FN), MO(_LW), MO(_RS) 
+ ),
+
   /* LOWER 
  * .-----------------------------------------------------.                      .-----------------------------------------------------.
  * | ESC    | 1      | 2      | 3      | 4      | 5      |                      | 6      | 7      | 8      | 9      | 0      | BACKSP |
@@ -169,13 +192,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
  [_FN] = LAYOUT( 
-    _______, RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI, _______,                        _______, _______, _______, _______, _______, _______ ,  
-    _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                          _______, KC_PGDN, KC_PGUP, _______, _______, _______ ,  
-    _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,                         KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, KC_PIPE, _______ ,  
-    _______, BL_TOGG, BL_STEP, RGB_TOG, KC_F11,  KC_F12,  _______,      _______, _______, _______, KC_LABK, KC_RABK, KC_QUES, _______ ,  
-                                        _______, _______, _______,      _______, _______, _______ 
+    _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                          _______, _______, _______, _______, _______, _______ ,  
+    _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,                         _______, KC_PGDN, KC_PGUP, _______, _______, _______ ,  
+    _______, RGB_MOD, RGB_HUI, RGB_SAI, KC_F11,  KC_F12,                         KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, KC_PIPE, _______ ,  
+    _______, BL_STEP, RGB_VAI, RGB_VAD, _______, _______, _______,      _______, _______, _______, KC_LABK, KC_RABK, KC_QUES, _______ ,  
+                                        _______, _______, TG(_GM),      _______, _______, _______ 
  )
- 
+
+
 };
 
 #define ENC_RD	PINF	//encoder port read
