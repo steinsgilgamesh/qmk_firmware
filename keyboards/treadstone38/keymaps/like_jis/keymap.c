4e1b0e9a71e51d1b872be0b5d7caa0d7bd65a72e
marksard <marksard@rocketmail.com>
2019-11-05 16:30:07 +0900

    treadstone38: modified keymap

--- keyboards/treadstone38/keymaps/like_jis/keymap.c
+++ keyboards/treadstone38/keymaps/like_jis/keymap.c
@@ -70,7 +70,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|---------+---------+---------+---------+---------+---------+---------+---------+---------+---------|
           KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,  KC_COMM,   KC_DOT,  KC_SLSH,
   //`---------+---------+---------+---------+---------+---------+---------+---------+---------+---------'
-       KC_LCTL,  KC_LALT,  KC_LGUI,            KC_BSLO,            KC_SPRA,    KANJI,   KC_TAB,   KC_ESC
+       KC_LCTL,  KC_LALT,  KC_LGUI,            KC_BSLO,            KC_SPRA,    KANJI,   KC_TAB,  KC_LALT
   //`---------+---------+---------+---------+---------+---------+---------+---------+---------+---------'
   ),
 
