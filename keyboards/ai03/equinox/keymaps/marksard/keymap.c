2d610062d82b723a193a17c6a76d27d2fa7c0b0c
marksard <marksard@rocketmail.com>
2020-03-02 09:16:28 +0900

    [equinox]add keymap

---
+++ keyboards/ai03/equinox/keymaps/marksard/keymap.c
@@ -0,0 +1,126 @@
+/* Copyright 2019 Ryota Goto
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include QMK_KEYBOARD_H
+/*
+    K000, K001, K002, K003, K004, K005, K006, K007, K008, K009, K010, K011, \
+    K100, K101, K102, K103, K104, K105, K106, K107, K108, K109, K110, K111, \
+    K200, K201, K202, K203, K204, K205, K206, K207, K208, K209, K210, K211, \
+    K300, K301, K302,     K304,       K306,       K308,   K309, K310, K311  \
+*/
+
+enum layer_number {
+  _BASE = 0,
+  _LOWER,
+  _RAISE,
+  _ADJUST,
+};
+
+enum custom_keycodes {
+  RGBRST = SAFE_RANGE,
+  LOWER,
+  RAISE,
+  KANJI,
+};
+
+#define KC_MLAD  MO(_ADJUST)
+#define KC_BSLO  LT(_LOWER, KC_BSPC)
+#define KC_SPRA  LT(_RAISE, KC_SPC)
+#define KC_ENSF  RSFT_T(KC_ENT)
+#define KC_ALAP  RALT_T(KC_APP)
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  [_BASE] = LAYOUT_all(
+  //,-----------------------------------------------------------------------------------------------------------.
+       KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_BSPC,
+  //|--------+--------+--------+--------+--------+--------|--------+--------+--------+--------+--------+--------|
+      KC_CAPS,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_ENSF,
+  //|--------+--------+--------+--------+--------+--------|--------+--------+--------+--------+--------+--------|
+      KC_LSFT, KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M, KC_COMM,  KC_DOT, KC_RSFT,
+  //|--------+--------+--------+--------+--------+--------|--------+--------+--------+--------+--------+--------|
+      KC_LCTL, KC_LALT, KC_LGUI,          KC_BSLO,          KC_MLAD,          KC_SPRA, KC_ALAP, KC_RALT,  KC_ESC
+  //`----------------------------------------------------------------------------------------------------------'
+  ),
+
+  [_LOWER] = LAYOUT_all(
+  //,-----------------------------------------------------------------------------------------------------------.
+      _______,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5, KC_MINS,  KC_EQL, KC_JYEN, KC_LBRC, KC_RBRC,  KC_DEL,
+  //|--------+--------+--------+--------+--------+--------|--------+--------+--------+--------+--------+--------|
+      _______,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10, XXXXXXX, XXXXXXX, XXXXXXX, KC_QUOT, KC_BSLS, _______,
+  //|--------+--------+--------+--------+--------+--------|--------+--------+--------+--------+--------+--------|
+      _______, _______,  KC_F11,  KC_F12, XXXXXXX,   KANJI,   KANJI,  KC_DEL, XXXXXXX,   KC_RO, KC_SLSH, _______,
+  //|--------+--------+--------+--------+--------+--------|--------+--------+--------+--------+--------+--------|
+      _______, _______, _______,          _______,          _______,          _______, _______, _______, _______
+  //`----------------------------------------------------------------------------------------------------------'
+  ),
+
+  [_RAISE] = LAYOUT_all(
+  //,-----------------------------------------------------------------------------------------------------------.
+      _______,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_MINS,
+  //|--------+--------+--------+--------+--------+--------|--------+--------+--------+--------+--------+--------|
+      _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, XXXXXXX, _______,
+  //|--------+--------+--------+--------+--------+--------|--------+--------+--------+--------+--------+--------|
+      _______, _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_COMM,  KC_DOT, KC_BSLS, _______,
+  //|--------+--------+--------+--------+--------+--------|--------+--------+--------+--------+--------+--------|
+      _______, _______, _______,          _______,          _______,          _______, _______, _______, _______
+  //`----------------------------------------------------------------------------------------------------------'
+  ),
+
+  [_ADJUST] = LAYOUT_all(
+  //,-----------------------------------------------------------------------------------------------------------.
+      XXXXXXX,   RESET,  RGBRST, AG_NORM, AG_SWAP, CG_TOGG, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  //|--------+--------+--------+--------+--------+--------|--------+--------+--------+--------+--------+--------|
+      XXXXXXX, RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, XXXXXXX, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, XXXXXXX, XXXXXXX,
+  //|--------+--------+--------+--------+--------+--------|--------+--------+--------+--------+--------+--------|
+      XXXXXXX, XXXXXXX, RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD, XXXXXXX, KC_BTN1, KC_BTN2, XXXXXXX, XXXXXXX, XXXXXXX,
+  //|--------+--------+--------+--------+--------+--------|--------+--------+--------+--------+--------+--------|
+      _______, _______, _______,          _______,          _______,          _______, _______, _______, _______
+  //`----------------------------------------------------------------------------------------------------------'
+  )
+};
+
+uint16_t get_tapping_term(uint16_t keycode) {
+  switch (keycode) {
+    case KC_BSLO:
+      return TAPPING_LAYER_TERM;
+    case KC_SPRA:
+      return TAPPING_LAYER_TERM;
+    default:
+      return TAPPING_TERM;
+  }
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+
+  bool result = false;
+  switch (keycode) {
+    case KANJI:
+      if (record->event.pressed) {
+        if (keymap_config.swap_lalt_lgui == false) {
+          register_code(KC_LANG2);
+        } else {
+          SEND_STRING(SS_LALT("`"));
+        }
+      } else {
+        unregister_code(KC_LANG2);
+      }
+      break;
+    default:
+      result = true;
+      break;
+  }
+
+  return result;
+}
