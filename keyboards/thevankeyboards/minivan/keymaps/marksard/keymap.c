594e44240f6667c803353cc87d8b159180cedc53
marksard <marksard@rocketmail.com>
2020-03-27 12:06:42 +0900

    [kumo]update

--- keyboards/thevankeyboards/minivan/keymaps/marksard/keymap.c
+++ keyboards/thevankeyboards/minivan/keymaps/marksard/keymap.c
@@ -34,71 +34,59 @@ enum tapdances{
 };
 
 // Layer Mode aliases
-// #define KC_TBSF  LSFT_T(KC_TAB)
-// #define KC_ROSF  RSFT_T(KC_RO)
 #define KC_ALAP  LALT_T(KC_APP)
 #define KC_ENSF  RSFT_T(KC_ENT)
+#define KC_ROSF  RSFT_T(KC_RO)
 
 // Layer tap
 #define KC_BSLO  LT(_LOWER, KC_BSPC)
 #define KC_SPRA  LT(_RAISE, KC_SPC)
 #define KC_MLAD  MO(_ADJUST)
 
-#define KC_SCCL  TD(TD_SCCL)
+// #define KC_SCCL  TD(TD_SCCL)
 
-qk_tap_dance_action_t tap_dance_actions[] = {
-  [TD_SCCL] = ACTION_TAP_DANCE_DOUBLE(KC_SCLN, KC_QUOT),
-};
+// qk_tap_dance_action_t tap_dance_actions[] = {
+//   [TD_SCCL] = ACTION_TAP_DANCE_DOUBLE(KC_SCLN, KC_QUOT),
+// };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [_BASE] = LAYOUT(
+  [_BASE] = LAYOUT_arrow(
   //,-----------------------------------------------------------------------------------------------------------.
-  //      Esc        Q        W        E        R        T        Y        U        I        O        P        -
-       KC_ESC,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_MINS,
+       KC_ESC,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_BSPC,
   //|--------+--------+--------+--------+--------+--------|--------+--------+--------+--------+--------+--------|
-  //Tab/Shift        A        S        D        F        G        H        J        K        L      ;/:    Enter
-       KC_TAB,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L, KC_SCCL, KC_ENSF,
+       KC_TAB,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_ENSF,
   //|--------+--------+--------+--------+--------+--------|--------+--------+--------+--------+--------+--------|
-  //    Shift        Z        X        C        V        B        N        M        ,        .        /       yen
-      KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,   KC_RO,
+      KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_ROSF,
   //|--------+--------+--------+--------+--------+--------|--------+--------+--------+--------+--------+--------|
-  //     Ctrl      Alt GUI/Comm            BackSpace/Lower                Space/Raise      Alt     Menu     Ctrl
-      KC_LCTL, KC_LALT, KC_LGUI,                   KC_BSLO,                   KC_SPRA, KC_RALT, KC_ALAP, KC_RCTL
+      KC_LCTL, KC_LALT, KC_LGUI,                   KC_BSLO,          KC_SPRA,  KC_APP, KC_RALT, KC_RGUI, KC_RCTL
   //`-----------------------------------------------------------------------------------------------------------'
   ),
 
-  [_LOWER] = LAYOUT(
+  [_LOWER] = LAYOUT_arrow(
   //,-----------------------------------------------------------------------------------------------------------.
-  //                F1       F2       F3       F4       F5        -        ^        \        @        [   Delete
       _______,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5, KC_MINS,  KC_EQL, KC_JYEN, KC_LBRC, KC_RBRC,  KC_DEL,
   //|--------+--------+--------+--------+--------+--------|--------+--------+--------+--------+--------+--------|
-  //                F6       F7       F8       F9      F10                          ;        :        ]
-      _______,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10, XXXXXXX, XXXXXXX, KC_SCLN, KC_QUOT, KC_BSLS, _______,
+      _______,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_QUOT, KC_BSLS,
   //|--------+--------+--------+--------+--------+--------|--------+--------+--------+--------+--------+--------|
-  //               F11      F12        Kana/Kanji    Enter   Delete
       _______,  KC_F11,  KC_F12, XXXXXXX,   KANJI,  KC_ENT,  KC_DEL, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|--------+--------+--------+--------+--------+--------|
-  //                                                                           Adjust
-      _______, _______, _______,                   _______,                   KC_MLAD, _______, _______, _______
+      _______, _______, _______,                   _______,          KC_MLAD, _______, _______, _______, _______
   //`-----------------------------------------------------------------------------------------------------------'
   ),
 
-  [_RAISE] = LAYOUT(
+  [_RAISE] = LAYOUT_arrow(
   //,-----------------------------------------------------------------------------------------------------------.
-  //                 1        2        3        4        5        6        7        8        9        0        -
       _______,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_MINS,
   //|--------+--------+--------+--------+--------+--------|--------+--------+--------+--------+--------+--------|
-  //                                                           Left     Down       Up    Right
       _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, XXXXXXX, _______,
   //|--------+--------+--------+--------+--------+--------|--------+--------+--------+--------+--------+--------|
-  //                                                                                ,        .        /      yen
       _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_COMM,  KC_DOT, KC_SLSH,   KC_RO,
   //|--------+--------+--------+--------+--------+--------|--------+--------+--------+--------+--------+--------|
-      _______, _______, _______,                   _______,                   _______, _______, _______, _______
+      _______, _______, _______,                   _______,          _______, _______, _______, _______, _______
   //`-----------------------------------------------------------------------------------------------------------'
   ),
 
-  [_ADJUST] = LAYOUT(
+  [_ADJUST] = LAYOUT_arrow(
   //,-----------------------------------------------------------------------------------------------------------.
   //             Reset LEDReset  MacMode  WinMode              Home PageDown   PageUp      End
       _______,   RESET,  RGBRST, AG_NORM, AG_SWAP, XXXXXXX, KC_HOME, KC_PGDN, KC_PGUP,  KC_END, XXXXXXX, XXXXXXX,
@@ -109,7 +97,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //           LED Mode Hue/Saturation/Value Decrement      Mouse Button Left Right
       _______, RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD, XXXXXXX, KC_BTN1, KC_BTN2, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|--------+--------+--------+--------+--------+--------|
-      _______, _______, _______,                   _______,                   _______, _______, _______, _______
+      _______, _______, _______,                   _______,          _______, _______, _______, _______, _______
   //`-----------------------------------------------------------------------------------------------------------'
   )
 };
