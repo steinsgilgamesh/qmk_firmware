f92ea301968153e31fa0586778b4c738d4ba8a32
marksard <marksard@rocketmail.com>
2020-03-27 12:07:49 +0900

    [treadstone60update]

--- keyboards/treadstone60/keymaps/like_jis/config.h
+++ keyboards/treadstone60/keymaps/like_jis/config.h
@@ -17,10 +17,10 @@
 #pragma once
 
 // place overrides here
-#define TAPPING_TERM 200
+#define TAPPING_TERM 190
 #define IGNORE_MOD_TAP_INTERRUPT
 
-#define TAPPING_LAYER_TERM 150 // Custom LT Tapping term
+#define TAPPING_LAYER_TERM 140 // Custom LT Tapping term
 #define TAPPING_TERM_PER_KEY
 
 #ifdef MOUSEKEY_ENABLE
--- keyboards/treadstone60/keymaps/like_jis/keymap.c
+++ keyboards/treadstone60/keymaps/like_jis/keymap.c
@@ -35,8 +35,10 @@ enum custom_keycodes {
   KANJI,
 };
 
-#define KC_CPSF  LSFT_T(KC_CAPS)
+#define KC_CPCT  LCTL_T(KC_CAPS)
 #define KC_ENSF  RSFT_T(KC_ENT)
+#define KC_ROSF  RSFT_T(KC_RO)
+#define KC_ALAP  RALT_T(KC_APP)
 
 #define KC_BSLO  LT(_LOWER, KC_BSPC)
 #define KC_SPLO  LT(_LOWER, KC_SPC)
@@ -49,11 +51,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------|
         KC_TAB,     KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,  KC_LBRC,  KC_BSPC,
   //|---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------|
-       KC_CPSF,     KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,  KC_SCLN,  KC_QUOT,  KC_ENSF,
+       KC_CPCT,     KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,  KC_SCLN,  KC_QUOT,  KC_ENSF,
   //|---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------|
-       KC_LSFT,     KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,  KC_COMM,   KC_DOT,  KC_SLSH,    KC_RO,  KC_RSFT,
+       KC_LSFT,     KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,  KC_COMM,   KC_DOT,  KC_SLSH,  KC_ROSF,  KC_ROSF,
   //|---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------|
-       KC_LCTL,  KC_LALT,  KC_LGUI,            KC_BSLO,            KC_SPLO,            KC_SPLO,  KC_AJST,    KANJI,   KC_APP,  KC_RCTL
+       KC_LCTL,  KC_LALT,  KC_LGUI,            KC_BSLO,            KC_SPLO,            KC_SPLO,  KC_AJST,  KC_ALAP,   KC_APP,  KC_RCTL
   //`---------------------------------------------------------------------------------------------------------------------------------'
   ),
 
