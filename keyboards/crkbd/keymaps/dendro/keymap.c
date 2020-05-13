d03fc4fd26814c1c08bf48aac20bdfce19fd8779
xuruiqi <ruiqi.xu@ed.ac.uk>
2020-05-13 15:11:05 +0800

    minor changes

--- keyboards/crkbd/keymaps/dendro/keymap.c
+++ keyboards/crkbd/keymaps/dendro/keymap.c
@@ -35,9 +35,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
   KC_TAB,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_UP, KC_L,   KC_ENT, \
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-  KC_LSFT,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_LEFT,  KC_DOWN, KC_RGHT, KC_RGUI, \
+  KC_LSFT,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_LEFT,  KC_DOWN, KC_RGHT, NAVI, \
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-  KC_LALT, KC_LCTL,  SYMBOL,     KC_SPC, NAVI,  KC_RCTL \
+  KC_LALT, KC_LCTL,  SYMBOL,     KC_SPC, KC_RGUI,  KC_RCTL \
   //`--------------------------'  `--------------------------'
                     ),
 
@@ -47,7 +47,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
   KC_TAB,    KC_4,    KC_5,    KC_6, XXXXXXX, XXXXXXX,                          XXXXXXX,  KC_GRV, KC_SCLN, KC_QUOT, KC_BSLS,  KC_ENT, \
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-  KC_LSFT,    KC_1,    KC_2,    KC_3, XXXXXXX, XXXXXXX,                         XXXXXXX, XXXXXXX, KC_COMM,  KC_DOT, KC_SLSH, KC_RGUI, \
+  KC_LSFT,    KC_1,    KC_2,    KC_3, XXXXXXX, XXXXXXX,                         XXXXXXX, XXXXXXX, KC_COMM,  KC_DOT, KC_SLSH, NAVI, \
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
   KC_LALT, KC_LCTL, _______,      KC_SPC, _______,    KC_RCTL \
   //`--------------------------'  `--------------------------'
@@ -59,7 +59,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
   KC_TAB,   KC_F5,   KC_F6,   KC_F7,   KC_F8,  KC_DEL,                          XXXXXXX, XXXXXXX, KC_HOME, _______, KC_END,  KC_ENT, \
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-  KC_LSFT,   KC_F1,   KC_F2,   KC_F3,   KC_F4,  KC_INS,                         XXXXXXX, XXXXXXX, C(KC_LEFT), _______, C(KC_RGHT), KC_RGUI, \
+  KC_LSFT,   KC_F1,   KC_F2,   KC_F3,   KC_F4,  KC_INS,                         XXXXXXX, XXXXXXX, C(KC_LEFT), _______, C(KC_RGHT), NAVI, \
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
   KC_LALT, KC_LCTL, _______,      KC_SPC, _______,  KC_RCTL \
   //`--------------------------'  `--------------------------'
@@ -71,7 +71,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
   KC_TAB, KC_MS_L, KC_MS_D, KC_MS_R, KC_WH_D, XXXXXXX,                          KC_BTN2, KC_BRID, KC_MPRV, KC_VOLD, KC_MNXT, KC_BTN2, \
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-  KC_LSFT, KC_ACL0, KC_ACL1, KC_ACL2, XXXXXXX, XXXXXXX,                         KC_BTN3, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_RGUI, \
+  KC_LSFT, KC_ACL0, KC_ACL1, KC_ACL2, XXXXXXX, XXXXXXX,                         KC_BTN3, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, NAVI, \
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
   KC_LALT, KC_LCTL, _______,      KC_SPC, _______,  KC_RCTL \
   //`--------------------------'  `--------------------------'
@@ -198,12 +198,12 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       crkbd_update_tri_layer(_SYMBOL, _NAVI, _FUNC);
     }
     return false;
-  // case FUNC:
-  //   if (record->event.pressed) {
-  //     layer_on(_FUNC);
-  //   } else {
-  //     layer_off(_FUNC);
-  //   } return false;
+    // case FUNC:
+    //   if (record->event.pressed) {
+    //     layer_on(_FUNC);
+    //   } else {
+    //     layer_off(_FUNC);
+    //   } return false;
     // case LOWER:
     //   if (record->event.pressed) {
     //     layer_on(_LOWER);
